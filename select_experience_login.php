e3a53f9c93eb00862e89a380c2c2b85347e97a43
zhengjie <dalaba9453@gmail.com>
2023-06-07 17:00:36 +0800

    second

--- select_experience_login.php
+++ select_experience_login.php
@@ -24,77 +24,7 @@ $result = mysqli_query($link, $sql);
 </head>
 
 <body>
-	<header class="header-area">
-		<!-- Navbar -->
-		<div class="navbar fixed top-0 left-0 right-0 z-50 bg-base-100 shadow-lg border-none">
-			<div class="navbar-start">
-				<div class="dropdown">
-					<label tabindex="0" class="btn btn-ghost btn-circle">
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-							stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-								d="M4 6h16M4 12h16M4 18h7" />
-						</svg>
-					</label>
-					<ul tabindex="0"
-						class="menu menu-compact dropdown-content mt-5 p-2 shadow-lg bg-base-100 rounded-box w-52">
-						<li><a href="homepage_login.php">主頁</a></li>
-						<li tabindex="0">
-							<span>課程</span>
-							<ul class="rounded-box bg-base-100 p-2 bg">
-								<li><a href="select_course_login.php">課程大綱</a></li>
-								<li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">課程影音</a></li>
-								<li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">課程教材</a></li>
-							</ul>
-						</li>
-						<li><a href="select_prize_login.php">獲獎紀錄</a></li>
-						<li><a href="select_book_login.php">個人著作</a></li>
-						<li><a href="select_experience_login.php">個人經歷</a></li>
-						<li><a href="select_periodical_login.php">期刊論文</a></li>
-						<li><a href="select_session_login.php">會議論文</a></li>
-						<li><a href="select_plan_login.php">計畫</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="navbar-center">
-				<a class="btn btn-ghost normal-case text-xl">李榮三主任個人網頁</a>
-			</div>
-			<div class="navbar-end">
-				<label for="my-modal-3" class="btn btn-ghost btn-circle">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-						stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-					</svg>
-				</label>
-				<div class="dropdown dropdown-end">
-					<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-						<div class="w-10 rounded-full">
-							<img src="https://www.iecs.fcu.edu.tw/media/img/teacher/avatar/899631.jpg" />
-						</div>
-					</label>
-					<ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-						<li><a href="homepage_setting.php">Setting</a></li>
-						<li><a href="index.php">Logout</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</header>
-	<!-- End-Navbar -->
-
-	<!-- Search Modal -->
-	<input type="checkbox" id="my-modal-3" class="modal-toggle">
-	<div class="modal">
-		<div class="modal-box relative">
-			<label for="my-modal-3"
-				class="btn btn-sm btn-circle border-none bg-base-100 text-black font-bold absolute right-2 top-2">✕</label>
-			<div class="form-control pr-6">
-				<input type="text" placeholder="Search" class="input input-bordered bg-base-100 ">
-			</div>
-		</div>
-	</div>
-
+	<?php include("header_login.php"); ?>
 	<div class="w-5/6 mx-auto mt-24">
 		<table class="min-w-full">
 			<thead class="bg-gray-300">
